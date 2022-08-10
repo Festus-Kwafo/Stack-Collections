7599e38448aa067411dfb3e2cbcb5f9ad3550571
Festus <festuskwafo11@gmail.com>
2022-08-10 10:01:01 +0000

    stack assignment

--- src/main/java/com/example/stackassignment/HelloApplication.java
+++ src/main/java/com/example/stackassignment/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-        stage.setTitle("Stack Assigment");
+        stage.setTitle("Stack Assignment - 10821161");
         stage.setScene(scene);
         stage.show();
     }
